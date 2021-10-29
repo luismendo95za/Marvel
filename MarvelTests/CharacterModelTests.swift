597c8254a0d31ac28fe66bdefff3ac9388db5217
Luis <>
2021-10-29 12:45:36 +0200

    * Update Unit Tests

--- MarvelTests/CharacterModelTests.swift
+++ MarvelTests/CharacterModelTests.swift
@@ -12,24 +12,19 @@ import XCTest
 class CharacterModelTests: BaseTest {
     
     func testsTopUpRecordParser() throws {
-        let model: Datum? = try getJSONObject(bundle: bundle, for: "result")
-        
-        XCTAssertNotNil(model)
-        XCTAssertNotNil(model?.count)
-        XCTAssertNotNil(model?.offset)
+        let decoder = JSONDecoder()
+        let json: Data = try getJSONObject(bundle: bundle, for: "result")
+        let rootModel = try? decoder.decode(RootModel.self, from: json)
+        XCTAssertNotNil(rootModel)
+        XCTAssertNotNil(rootModel?.data?.count)
+        XCTAssertNotNil(rootModel?.data?.offset)
     }
     
-    
-    func getJSONObject<T:Glossy>(bundle: Bundle, for jsonName: String) throws -> T? {
-        guard let json = try getJSON(bundle: bundle, for: jsonName) else { return nil }
-        return T.init(json: json)
-    }
-    
-    func getJSON(bundle: Bundle, for jsonName: String) throws -> JSON? {
+    func getJSONObject(bundle: Bundle, for jsonName: String) throws -> Data {
         guard let path = bundle.path(forResource: jsonName, ofType: "json") else {
             fatalError("Could not retrieve file \(jsonName).json")
         }
         let data = try Data(contentsOf: URL(fileURLWithPath: path))
-        return try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? JSON
+        return data
     }
 }
